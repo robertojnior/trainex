22bd0bc3f19cc4a7c080fdcc6586c6582ef6be38
José Roberto <robertojunior085@gmail.com>
2021-06-24 00:19:45 -0300

    Setup absinthe graphql

--- lib/trainex_web/router.ex
+++ lib/trainex_web/router.ex
@@ -9,6 +9,13 @@ defmodule TrainexWeb.Router do
     pipe_through :api
   end
 
+  scope "/api" do
+    pipe_through :api
+
+    forward "/graphql", Absinthe.Plug, schema: TrainexWeb.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: TrainexWeb.Schema
+  end
+
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Trainex.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:absinthe, "~> 1.5.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
